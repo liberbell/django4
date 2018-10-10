70eb75ffdb8ef01d4cc3013e1a16a42f773e60e5
liberbell <liberbell@gmail.com>
2018-10-11 08:26:00 +0900

    view1

--- mySite/myapp/views.py
+++ mySite/myapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from myapp.forms import KakikomiForm
 
 # Create your views here.
 def index_template(response):
     return render(request, 'index.html')
+
+def kakikomi(request):
+    f = KakikomiForm()
+    return HttpResponse(f)
